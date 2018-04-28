1533c6ab1f24a71047c4448714854e5b97dc8d8b
Natalie Halloran <natalie.halloran@yahoo.com>
2018-04-28 19:30:09 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+var recipes = {}
+function updateObjectWithKeyAndValue(object,key,value) {
+  Object.assign({},object,key,value)
+  return object
+}
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
+  object[key] = value
+  return object
+}
+function destructivelyDeleteFromObjectByKey(object,key) {
+  delete object[key]
+  return object
+}
+function deleteFromObjectByKey(object,key) {
+  var newObj = Object.assign({},object,key)
+  delete newObj[key]
+  return newObj
+}
